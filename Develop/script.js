15080c66d77fbb403ce5c69a1939ee52f096155f
giovannakuan <gk2570@tc.columbia.edu>
2022-01-08 16:03:07 -0500

    global variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,11 @@
 // Assignment code here
 
+// Global Variables
+var arraySplit = ""; // Used to translate input array back to the user as a string with slice
+var exitApplication = false; // on/off switch for running the app
+var userInput = ""; // Important - will be used to generate password based on criteria
+var finalPassword = ""; // Important - used as a variable to store temporatly a random passoword.
+var displayPassword = ""; // Impoprtant- used to display the final password on the text area of the HTML element.
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
